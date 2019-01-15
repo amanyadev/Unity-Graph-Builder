a7cfb185e9d44ede6462fd2b484558b2f50bd78c
Aman Kumar Yadav <yadav.aman099@gmail.com>
2019-01-16 01:01:13 +0530

    new files

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{   
+    [Range(10,100)] public int resolution = 10;
+    
+    public Transform pointPrefab;
+
+    void Awake(){
+        float step = 2f / resolution;
+       	Vector3 scale = Vector3.one *step;
+		Vector3 position;
+        position.x = 0;
+        position.y = 0;
+        position.z = 0;
+		for (int i = 0; i < resolution; i++) {
+			Transform point = Instantiate(pointPrefab);
+
+			position.x = (i + 0.5f) * step - 1f;//adjusting cubes
+            position.y = (position.x)*position.x*position.x;
+			point.localPosition = position;
+			point.localScale = scale;
+            point.SetParent(transform);
+            
+		}
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
