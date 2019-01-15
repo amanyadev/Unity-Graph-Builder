3220848806c933e531ca63cc0f3a8c3e360884dc
Aman Kumar Yadav <yadav.aman099@gmail.com>
2019-01-16 01:11:58 +0530

    adding animation to the graph

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -4,32 +4,29 @@ using UnityEngine;
 
 public class Graph : MonoBehaviour
 {   
+    Transform[] points; 
     [Range(10,100)] public int resolution = 10;
     
     public Transform pointPrefab;
 
-    void Awake(){
+    void Start(){
         float step = 2f / resolution;
        	Vector3 scale = Vector3.one *step;
 		Vector3 position;
         position.x = 0;
         position.y = 0;
         position.z = 0;
+        points = new Transform[resolution];
 		for (int i = 0; i < resolution; i++) {
 			Transform point = Instantiate(pointPrefab);
-
+            points[i] = point;
 			position.x = (i + 0.5f) * step - 1f;//adjusting cubes
-            position.y = (position.x)*position.x*position.x;
+         //   position.y = (position.x)*position.x*position.x;
 			point.localPosition = position;
 			point.localScale = scale;
             point.SetParent(transform);
             
 		}
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
     }
 
     // Update is called once per frame
